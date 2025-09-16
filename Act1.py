c5d9b9d2532b9962122c2b76eff972330a006dd8
Jade Celestial <michaelcelestial16@gmail.com>
2025-09-16 09:09:16 -0600

    1st version of the commit

---
+++ Act1.py
@@ -0,0 +1,8 @@
+x = 3
+y = 4.5
+message = "hello"
+msg = "hola"
+print(x)
+print(y)
+print(msg)
+print(message)
---
+++ myAct3.py
@@ -0,0 +1,12 @@
+numbers = 123
+numbers_decimal = 32.2
+numbers_float = 3/4
+text = "hello, this is an example of string"
+text1 = 'this is a text'
+boolean = True
+
+print (numbers)
+print(numbers_decimal)
+print(text)
+print(text1)
+print(boolean)
