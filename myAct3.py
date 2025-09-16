2a032b90323d248110614acb3b27e51638656fd4
cmrc-ui <michaelcelestial16@gmail.com>
2025-09-16 10:20:12 -0600

    adding another print statement

--- myAct3.py
+++ myAct3.py
@@ -4,9 +4,10 @@ numbers_float = 3/4
 text = "hello, this is an example of string"
 text1 = 'this is a text'
 boolean = True
-
+y = 4.5
 print (numbers)
 print(numbers_decimal)
 print(text)
 print(text1)
 print(boolean)
+print(y)
