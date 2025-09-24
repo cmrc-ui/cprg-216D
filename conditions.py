a401eded1f7f200bae1e28062e3721e4a8b1e73b
cmrc-ui <michaelcelestial16@gmail.com>
2025-09-23 19:09:45 -0600

    practicing conditions/quadratic

---
+++ conditions.py
@@ -0,0 +1,32 @@
+"""
+#Boolean
+
+condition = True #or
+cond = False 
+
+#ideal variable name for Boolean should be atleast "is_true   " or "has_data" #or "can_edit"
+#example
+is_Old = True
+has_data = False
+isMale = True
+
+"""
+
+"""
+
+user_input = int(input("Please enter your age: "))
+if user_input == 18:
+    print("You are a young adult")
+elif user_input > 18:
+    print("You are an adult")
+else:                  
+    print("You are a minor")
+"""
+a = 0 
+b= 2
+c = 4
+quadratic = int(b**2 - 4*a*c)   
+print(quadratic >= 0)
+if quadratic >= 0:
+    print("Real Roots")
+elif quadratic >
