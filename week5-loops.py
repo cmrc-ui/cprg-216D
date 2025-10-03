ce6b41ec93e365f24c2397b2a9a962bb1fb74407
cmrc-ui <michaelcelestial16@gmail.com>
2025-10-03 16:47:39 -0600

    practice on while

---
+++ week5-loops.py
@@ -0,0 +1,33 @@
+"""
+while statemnt
+
+if condition:  #runs only once if the condition is true
+    statement1
+    statement2
+
+while condition: #the statement run as long as the condition is true
+    statement1
+    statement2
+
+"""
+
+ """Sum of 1 to 100
+sum = 0
+counter = 1 
+while counter <= 100:
+    sum+= counter
+    counter += 1
+print("sum of first 100 natural numbers is:", sum)
+"""
+"""
+'''Factorial of a number'''
+factorial = 1
+counter = 1
+while counter <=10:
+    factorial *= counter
+    counter += 1
+print("Factorial of 10 is:", factorial)
+"""
+
+'''Quadtratic Solver'''
+
