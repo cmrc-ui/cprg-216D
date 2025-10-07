3eddc4f73d3dbe64dff36cdc1d5cefcc4e8f9112
cmrc-ui <michaelcelestial16@gmail.com>
2025-10-06 22:20:38 -0600

    practice on while and basic code

--- week5-loops.py
+++ week5-loops.py
@@ -11,7 +11,7 @@ while condition: #the statement run as long as the condition is true
 
 """
 
- """Sum of 1 to 100
+"""Sum of 1 to 100
 sum = 0
 counter = 1 
 while counter <= 100:
@@ -19,8 +19,9 @@ while counter <= 100:
     counter += 1
 print("sum of first 100 natural numbers is:", sum)
 """
+
 """
-'''Factorial of a number'''
+Factorial of a number
 factorial = 1
 counter = 1
 while counter <=10:
@@ -29,5 +30,70 @@ while counter <=10:
 print("Factorial of 10 is:", factorial)
 """
 
-'''Quadtratic Solver'''
+""" 
+#1 to 10 Counter
+counter = 1
+while counter <= 10:
+    print(counter)
+    counter += 1
+"""
+"""
+#Print even numbers from 2 to 20
+counter = 2
+while counter <= 20:
+    if counter %2 == 0:
+        print(counter)
+    counter += 2
+"""
+"""
+#Even COunter
+counter = 0
+while True:
+    number = input("Enter a number(or exit to quit):")
+    if number.lower() == "exit":
+        print("program ends")
+        break
+    number = int(number)
+    if number %2 == 0:
+        counter += 1
+    else:
+        pass
+print("total number of even:",counter)
+"""
+"""
+#Printing the largest number
+number1 = int(input("First Number: "))
+number2 = int(input("Second Number: "))
+number3 = int(input("Third Number: "))
 
+highest = number1
+
+if(number2 > highest):
+    highest = number2
+    if(number3 > highest):
+        highest = number3
+print(highest)
+"""
+"""
+#if passer
+grade = input("Input the Grade: ")
+grade = float(grade)
+if grade >= 90 and grade <= 100:
+    print("A")
+elif grade <= 89 and grade >= 80:
+    print("B")
+elif grade <= 60:
+    print("Failed")
+"""
+number = input("Any number:")
+number = int(number)
+counter = 1
+while counter <= number:
+    counter += 1
+    print(counter-1)
+    
+
+
+
+
+    
