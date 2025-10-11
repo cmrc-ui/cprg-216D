0f86c2ada9cb86b19edb0a60368ce3194ce0479d
cmrc-ui <michaelcelestial16@gmail.com>
2025-10-10 19:32:48 -0600

    Update on assignment1

--- Assignment.py
+++ Assignment.py
@@ -1,19 +1,33 @@
 #The Assignment
 print("Welcome to the Grade Registry Program")
-new = input("Would you like to add a new student?y(yes)),n(n0): ")
-if new.lower() or new.upper() == 'yes':
-    new_student = input("Enter the student's name:")
-    print(new_student)
-    print("Enter the student GPA for each subject. Enter -1 to stop entering GPA")
-    GPA_count = 0
-    total_GPAsum = 0
-    GPA_input = float(input())
-    while GPA_input <= 4.0 and GPA_input > -1:
-        total_GPAsum += GPA_input
-        GPA_count += 1
+positive_answer = ['y','yes','yEs', 'YES', 'Yes']
+negative_answer = ['no','n']
+new = input("Would you like to add a new student?y(yes),n(n0): ").lower()
+if new in positive_answer:
+    while True:
+        new_student = input("Enter the student's name:")
+        print(new_student)
+        print("Enter the student GPA for each subject. Enter -1 to stop entering GPA")
+        GPA_count = 0
+        total_GPAsum = 0
         GPA_input = float(input())
-    GPA_count -=1
-    print(total_GPAsum/GPA_count)
+        while -1 <GPA_input <4.0:
+            total_GPAsum += GPA_input
+            GPA_count += 1
+            GPA_input = float(input())
+        if GPA_count > 0:
+            print(total_GPAsum/GPA_count)
+        elif GPA_input > 4:
+            print("Invalid GPA")
+        else:
+            print("No GPA Input")
+        new = input("Would you like to add a new student?y(yes)),n(n0): ")
+        while new not in positive_answer and new not in negative_answer:
+            print("Incorrect Input, please enter y(yes),n(no)")
+            new = input("Would you like to add a new student?y(yes)),n(n0): ")
+        if new in negative_answer:
+            print("DONE")
+            break
         
         
     
