d97c42fc1ed5032f701accadcdffb7571d1c86ae
cmrc-ui <michaelcelestial16@gmail.com>
2025-10-17 01:32:54 -0600

    Clydes copy of assignment

--- Assignment.py
+++ Assignment.py
@@ -11,25 +11,25 @@ if new in positive_answer:
         GPA_count = 0
         total_GPAsum = 0
         GPA_input = float(input())
-        while -1 <GPA_input <4.0:
-            total_GPAsum += GPA_input
-            GPA_count += 1
-            GPA_input = float(input())
-        if GPA_count > 0:
-            print(total_GPAsum/GPA_count)
-        elif GPA_input > 4:
-            print("Invalid GPA")
-        else:
-            print("No GPA Input")
+        if GPA_input == -1:
+            break
+        while GPA_input != -1:
+            if GPA_input > 4:
+                print("Invalid GPA")
+            else:
+                total_GPAsum += GPA_input
+                GPA_count += 1
+                GPA_input = float(input())     
+        print(new_student,"'s GPA is: ",format(total_GPAsum/GPA_count,".2f"))           
         new = input("Would you like to add a new student?y(yes)),n(n0): ")
         while new not in positive_answer and new not in negative_answer:
             print("Incorrect Input, please enter y(yes),n(no)")
             new = input("Would you like to add a new student?y(yes)),n(n0): ")
         if new in negative_answer:
-            print("DONE")
-            break
-        
-        
+                print("GPA Calculator Ends")
+                break
+
+            
     
 
 
